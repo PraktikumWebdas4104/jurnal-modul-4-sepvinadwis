4bb6108366b467b915892c909237a73ec5adf9fc
sepvinadwis <43513131+sepvinadwis@users.noreply.github.com>
2018-09-28 09:51:16 +0700

    Update formawal.php
    SEPVINA DWI SANTOSA
    6701174066

--- formawal.php
+++ formawal.php
@@ -1,5 +1,25 @@
 <?php
-
-
-
+<form action="proses.php" method="POST">
+	<table> 
+		<tr>
+			<td>USERNAME</td>
+			<td><input type="text" name="username"></td>
+		</tr>
+		<tr>
+			<td>PASSWORD</td>
+			<TD><input type="text" name="password"></TD>
+		</tr>
+				<tr>
+			<td>upload foto</td>
+			<td><input type = "file" name="upload foto" placeholder="upload foto"></br>
+			</tr>
+			<tr>
+				<td>hapus foto</td>
+				<td><input type ="submit" name="buttons" value="delete"></td>
+				</tr>
+		<tr>
+			<td><input type="submit" name="submit" value="LOGIN"></td>>
+		</tr>
+	</table>
+</form>
 ?>
