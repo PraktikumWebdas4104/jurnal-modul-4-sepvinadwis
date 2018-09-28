34006d771f4339e8e674bdfadce37e2ee9012b76
sepvinadwis <43513131+sepvinadwis@users.noreply.github.com>
2018-09-28 10:02:32 +0700

    Update proses.php
    SEPVINA DWI SANTOSA
    6701174066
    D3MI-41-04

--- proses.php
+++ proses.php
@@ -1,4 +1,43 @@
 <?php
+error_reporting(0);
+	$usseer=$_POST['username'];
+	$pass=$_POST['password'];
 
+	$user= array("user" => "sepvina",
+					"password" => "1998");
 
+	if(isset($_POST["submit"])){
+		if($usseer==$user['user'] && $pass==$user['password']){
+			echo "LOGIN".$usseer."Berhasilll<br><br>";
+
+			echo "<form method='POST'>
+					<input type='checkbox' name='hobi[]'' value='Bersepeda'> Bersepeda <br><br>
+					<input type='checkbox' name='hobi[]' value='Nonton'> Nonton <br><br>
+					<input type='checkbox' name='hobi[]' value='Makan'> Makan <br><br>
+					<input type='checkbox' name='hobi[]' value='Futsal'> Futsal <br><br>
+					<input type='checkbox' name='hobi[]' value='Berenang'> Berenang <br><br>
+					<input type='checkbox' name='hobi[]' value='Ngegame'> Ngegame <br><br>
+					<input type='checkbox' name='hobi[]' value='Lari dari Kenyataan'> Lari dari Kenyataan <br><br>
+					<input type='submit' name='submit2'>
+			</form>";
+		}
+		else{
+			echo "USERNAME ATAU PASSWORD SALAH!!!!";
+		}
+		
+	}
+ ?>
+
+<?php
+ if(isset($_POST['submit2'])){
+ 	$data=$_POST['hobi'];
+ 	echo"Hobby :";
+ 	foreach ($data as $nilai) {
+ 		echo "$nilai,";
+
+ 		# code...
+ 	}
+
+ 	echo "<br>Gambar: <br>";
+ }
 ?>
